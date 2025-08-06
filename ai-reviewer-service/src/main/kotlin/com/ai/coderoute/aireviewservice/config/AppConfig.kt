71f0e50d69292bd55caa837c5d4f0c5d350bb016
Faizan Mir <47100489+faizanmir@users.noreply.github.com>
2025-08-06 14:47:22 +0530

    Fixes

--- ai-reviewer-service/src/main/kotlin/com/ai/coderoute/aireviewservice/config/AppConfig.kt
+++ ai-reviewer-service/src/main/kotlin/com/ai/coderoute/aireviewservice/config/AppConfig.kt
@@ -1,13 +1,42 @@
 package com.ai.coderoute.aireviewservice.config
 
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.ChatClientRequest
+import org.springframework.ai.chat.client.ChatClientResponse
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain
+import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class AppConfig {
     @Bean
-    fun chatClient(builder: ChatClient.Builder): ChatClient {
-        return builder.build()
+    fun chatClient(
+        builder: ChatClient.Builder,
+        ollamaChatModel: OllamaChatModel,
+    ): ChatClient {
+        return builder
+            .build()
+    }
+
+    @Bean
+    fun noopCallAdvisor(): CallAdvisor {
+        return object : CallAdvisor {
+            override fun adviseCall(
+                chatClientRequest: ChatClientRequest,
+                callAdvisorChain: CallAdvisorChain,
+            ): ChatClientResponse {
+                return callAdvisorChain.nextCall(chatClientRequest)
+            }
+
+            override fun getName(): String {
+                return "NoOpCallAdvisor"
+            }
+
+            override fun getOrder(): Int {
+                return Int.MAX_VALUE
+            }
+        }
     }
 }
